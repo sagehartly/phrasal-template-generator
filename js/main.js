b02cbbbcce6a64f2ec4d2315d2b41525d5e98ad0
sagehartly <sage.hartly@gmail.com>
2016-02-26 17:14:57 -0500

    started madly

---
+++ js/main.js
@@ -0,0 +1,11 @@
+alert('Let’s make a story together!')
+
+var aColour = prompt('A colour (hot pink, indigo, etc.)');
+
+var aCreature = prompt('A creature (gorilla, moth, etc.)');
+
+var anAdjective = prompt('An adjective (ugly, stupendous)');
+
+var aVerb = prompt('A past tense verb (painted, coded)');
+
+document.write ('After snacking on ' + anAdjective + ' treats, the ' + aColour + ' bellied ' + aCreature + ' ' + aVerb + ' for hours.')
